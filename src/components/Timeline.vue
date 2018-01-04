2460a30aaa1273154c3db86d71ed4bc8bd456c48
Andy Shora <andyshora@gmail.com>
2018-01-04 18:40:33 +0000

    Timeline working

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,10 +1,27 @@
 <template>
   <div class='timeline'>
+    <div class='timeline__day' v-for='d in data' :key='d.day'>
+      <h1>{{ d.day }}</h1>
+      <div class='timeline__notes'>
+        <span class='tag' v-for='tag in d.overnightNotes'>{{ tag }}</span>
+      </div>
+      <div class='timeline__notes'>
+        <span class='tag' v-for='tag in d.trainingFocus'>{{ tag }}</span>
+      </div>
+      <div v-bind:class='{
+        "timeline__block": true,
+        "timeline__block--active": l.events.length
+        }' v-for='l in d.lines'>
+        <p v-if='l.events.length'>{{ formatDate(l.time) }}</p>
+        <div v-bind:class='["timeline__event timeline__event--" + e]' v-for='e in l.events'>{{ e }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
+import moment from 'moment';
 
 export default {
   components: {},
@@ -16,11 +33,55 @@ export default {
   methods: {
     ...mapActions([
       'fetchData'
-    ])
+    ]),
+    formatDate: dateStr => {
+      return moment(dateStr).format('hh:mm');
+    }
   }
 }
 </script>
 
-<style scoped>
-.timeline {}
+<style scoped lang="scss">
+.timeline {
+  display: flex;
+  align-items: flex-start;
+}
+.timeline__day {
+  width: 100%;
+  margin: 0 10px;
+}
+.timeline__block {
+  height: 100px;
+  margin: 10px;
+  border: 1px dashed rgba(0, 0, 255, 0.1);
+
+  > p {
+    margin: 0;
+  }
+}
+.timeline__block--active {
+}
+.timeline__notes {
+  height: 50px;
+}
+.timeline__event {
+
+}
+.timeline__event--poo {
+  background: brown;
+  color: white;
+}
+.timeline__event--wee {
+  background: yellow;
+  color: black;
+}
+.timeline__event--meal {
+  background: blue;
+  color: white;
+}
+.tag {
+  padding: 3px;
+  margin: 5px;
+  background: white;
+}
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import config from 'config';
+import moment from 'moment';
 
 Vue.use(Vuex);
 
@@ -14,14 +15,47 @@ export const store = new Vuex.Store({
   },
   mutations: {
     SET_DATA: (state, data) => {
-      // todo - parse CSV data
-      state.data = data;
+
+      // parse tsv data
+      const lines = data.split('\n');
+      const dates = lines.shift().replace('\r', '').split('\t');
+      const days = lines.shift().replace('\r', '').split('\t');
+      const trainingFocus = lines.shift().replace('\r', '').split('\t');
+      const overnightNotes = lines.shift().replace('\r', '').split('\t');
+      dates.shift();
+      days.shift();
+      trainingFocus.shift();
+      overnightNotes.shift();
+      const parsedLines = lines.map(l => l.split('\t'));
+      const getTimeline = (dayNumber, dateStr) => {
+
+        return parsedLines.map(l => {
+          // first index is the time of day
+          return {
+            time: moment(`${dateStr} ${l[0]}`, 'DD/MM hh:mm'),
+            events: l[dayNumber].replace('\r', '').split(',').filter(elm => !!elm.length)
+          };
+        })
+      };
+
+      const parsedData = dates.map((dateStr, i) => {
+        const m = moment(dateStr, 'DD/MM');
+        const dayNumber = Number.parseInt(days[i]);
+        return {
+          date: m,
+          day: dayNumber,
+          trainingFocus: trainingFocus[i].replace(/"/g, '').split(',').filter(elm => !!elm.length),
+          overnightNotes: overnightNotes[i].replace(/"/g, '').split(',').filter(elm => !!elm.length),
+          lines: getTimeline(dayNumber, dateStr)
+        };
+      });
+
+      state.data = parsedData;
     }
   },
   actions: {
     fetchData: ({ commit }) => {
       Vue.axios.get(config.sheetUrl).then((response) => {
-        console.log(response);
         commit('SET_DATA', response.data);
       });
     }
